767a6d65ad80a2f54334025169f476a040890c38
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-01-25 12:32:52 -0500

    rest logic

--- src/main/java/org/example/recipe/controllers/RecipeRestController.java
+++ src/main/java/org/example/recipe/controllers/RecipeRestController.java
@@ -1,6 +1,8 @@
 package org.example.recipe.controllers;
 
 import org.example.recipe.models.Recipe;
+import org.example.recipe.repositories.RecipeJpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,14 +11,30 @@ import java.util.List;
 
 public class RecipeRestController {
 
+    @Autowired
+    private RecipeJpaRepository jpaRepository;
+
     @GetMapping("")
-    public List<Recipe> listRecipes() {}
+    public List<Recipe> listRecipes() {
+        return jpaRepository.findAll();
+    }
     @GetMapping("/{id}")
-    public Recipe getRecipe(@PathVariable Long id) {}
+    public Recipe getRecipe(@PathVariable Long id) {
+        return jpaRepository.getReferenceById(id);
+    }
     @PostMapping("")
-    public Recipe createRecipe(@RequestBody Recipe recipe) {}
+    public Recipe createRecipe(@RequestBody Recipe recipe) {
+        return jpaRepository.save(recipe);
+    }
     @PutMapping("/{id}")
-    public Recipe updateRecipe(@RequestBody Recipe recipe, @PathVariable Long id) {}
+    public Recipe updateRecipe(@RequestBody Recipe recipe, @PathVariable Long id) {
+        Recipe update = jpaRepository.getReferenceById(id);
+        update.setName(recipe.getName());
+        update.setIngredients(recipe.getIngredients());
+        return  jpaRepository.save(update);
+    }
     @DeleteMapping("/{id}")
-    public Recipe deleteRecipe(@PathVariable Long id) {}
+    public Recipe deleteRecipe(@PathVariable Long id) {
+        return jpaRepository.getReferenceById(id);
+    }
 }
